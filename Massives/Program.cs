7d87d7771084985bbd5b5005f577f29b640d53bc
Bahdan Tsimashevich <timoh.manutd@gmail.com>
2019-04-22 16:45:53 +0300

    Add project files.

---
+++ Massives/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Massive.B6_P1_6_1DReadConsolemassive();
+           // Massive.B6_P2_6_3DMassiveMaxInRow();
+            Massive.B6_P3_6_1DMassiveSort();
+            Console.ReadLine();
+        }
+    }
+}
