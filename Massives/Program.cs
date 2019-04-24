76534d94ffd9d6ad40079a885ca7747047650b30
Bahdan Tsimashevich <timoh.manutd@gmail.com>
2019-04-24 19:53:16 +0300

    Fully completed homework6

--- Massives/Program.cs
+++ Massives/Program.cs
@@ -11,8 +11,13 @@ namespace Massive
         static void Main(string[] args)
         {
             //Massive.B6_P1_6_1DReadConsolemassive();
-           // Massive.B6_P2_6_3DMassiveMaxInRow();
-            Massive.B6_P3_6_1DMassiveSort();
+            //Massive.B6_P2_6_3DMassiveMaxInRow();
+            //Massive.B6_P3_6_1DMassiveSort();
+            //Massive.B6_P4_6_Pyatnashki();
+            //Massive.B6_P5_6_CutString();
+            Massive.B6_P6_6_ReplaceInPoem();
+
+
             Console.ReadLine();
         }
     }
